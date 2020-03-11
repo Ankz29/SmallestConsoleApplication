41e236660ed82c8cfe4e657d1a4fa69469b7fcf4
ankita_muddapur <ankita_muddapur@cms.co.in>
2020-03-11 12:48:27 +0530

    Adding files for smallest no. using console application.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmallestConsoleApplication;
+
+namespace SmallestConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            Console.Write( "enter the array elements: " + "\n" );
+
+            for (int i= 0 ; i< array.Length ; i++)
+            {
+                var arrayElements = Convert.ToInt32(Console.ReadLine());
+            }
+
+            var smallestElement = SmallestUtility.SmallestMethod( array );
+
+            Console.Write( "The smallest elements from the array is: " + smallestElement );
+            Console.Read();
+        }
+    }
+}
---
+++ SmallestUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallestConsoleApplication
+{
+   public static class SmallestUtility
+    {
+       public static int Smallest = 0; 
+        public static int SmallestMethod(int[] array)
+        {
+            Smallest = array[9];
+
+            for (int j = 0; j < array.Length; j++ )
+            {
+                if (array[j] > Smallest)
+                {
+                    Smallest = array[j];
+                }
+            }
+
+                return Smallest;
+        }
+    }
+}
